715d9ce76de9bc0482580a63f65169c6550372c1
K@S.Y <ferghus@protonmail.ch>
2026-04-25 09:33:13 +0900

    단어 수 단위 표기 오류 수정 - 표시 단위를 [개] → [자]로 변경

--- uAppStats.pas
+++ uAppStats.pas
@@ -62,25 +62,25 @@ begin
 
     SB.Append('<br>').Append(Bold('문자 종류:')).Append('<br>');
 
-    SB.AppendFormat('한글: <b>%s 개</b><br>', [AddComma(TextInfo.HangulCount)]);
-    SB.AppendFormat('(자음: <b>%s 개</b> / 모음: <b>%s 개</b>)<br><br>', [
+    SB.AppendFormat('한글: <b>%s 자</b><br>', [AddComma(TextInfo.HangulCount)]);
+    SB.AppendFormat('(자음: <b>%s 자</b> / 모음: <b>%s 자</b>)<br><br>', [
       AddComma(TextInfo.HangulConsonantCount),
       AddComma(TextInfo.HangulVowelCount)
     ]);
 
-    SB.AppendFormat('한자: <b>%s 개</b><br><br>', [AddComma(TextInfo.HanjaCharCount)]);
+    SB.AppendFormat('한자: <b>%s 자</b><br><br>', [AddComma(TextInfo.HanjaCharCount)]);
 
-    SB.AppendFormat('영문: <b>%s 개</b><br>', [AddComma(TextInfo.AsciiLetterCount)]);
-    SB.AppendFormat('(소문자: <b>%s 개</b> / 대문자: <b>%s 개</b>)<br><br>', [
+    SB.AppendFormat('영문: <b>%s 자</b><br>', [AddComma(TextInfo.AsciiLetterCount)]);
+    SB.AppendFormat('(소문자: <b>%s 자</b> / 대문자: <b>%s 자</b>)<br><br>', [
       AddComma(TextInfo.AsciiLowerCount),
       AddComma(TextInfo.AsciiUpperCount)
     ]);
 
-    SB.AppendFormat('숫자: <b>%s 개 </b><br>', [AddComma(TextInfo.AsciiDigitCount)]);
-    SB.AppendFormat('특수 문자: <b>%s 개 </b><br><br>', [AddComma(TextInfo.SpecialCharCount)]);
+    SB.AppendFormat('숫자: <b>%s 자 </b><br>', [AddComma(TextInfo.AsciiDigitCount)]);
+    SB.AppendFormat('특수 문자: <b>%s 자 </b><br><br>', [AddComma(TextInfo.SpecialCharCount)]);
 
-    SB.AppendFormat('공백: <b>%s 개 </b><br>', [AddComma(TextInfo.SpaceCount + TextInfo.OtherSpaceCount)]);
-    SB.AppendFormat('(스페이스: <b>%s 개</b> / 기타: <b>%s 개</b>)', [
+    SB.AppendFormat('공백: <b>%s 자 </b><br>', [AddComma(TextInfo.SpaceCount + TextInfo.OtherSpaceCount)]);
+    SB.AppendFormat('(스페이스: <b>%s 자</b> / 기타: <b>%s 자</b>)', [
       AddComma(TextInfo.SpaceCount),
       AddComma(TextInfo.OtherSpaceCount)
     ]);
